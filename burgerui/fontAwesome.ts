f29e39f07868d5ee39a0092a5de6a73091510ab2
Romeo509 <138832077+Romeo509@users.noreply.github.com>
2024-12-20 04:40:24 +0100

    Delete burgerui/fontAwesome.ts

--- burgerui/fontAwesome.ts
+++
@@ -1,4 +0,0 @@
-// fontAwesome.ts
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
-config.autoAddCss = false; // Tell Font Awesome to skip auto-adding CSS
